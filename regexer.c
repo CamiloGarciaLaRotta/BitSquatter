5d7878f709eddd057e18dcf995fe3944c7250dbd
cegal <camilo.garcia.larotta@gmail.com>
2018-09-02 10:22:36 -0400

    Fix Memory Leak
    Needed to free regex: regfree()
    
    Closes #5

--- regexer.c
+++ regexer.c
@@ -3,18 +3,24 @@
 // match the given pattern in the string to_match
 // the start and end position of the matched pattern are stored accordingly
 int match_regex(const char* pattern, const char* to_match, int* start, int* end)
-{	
+{
 	assert(start);
 	assert(end);
-	
+
 	regex_t r;
 	size_t nmatch = 2;
 	regmatch_t pmatch[2];
-	
+
 	int compile_status = regcomp(&r, pattern, REG_EXTENDED);
-	if(compile_status != 0) return compile_status;
+	if(compile_status != 0)
+	{
+		regfree(&r);
+		return compile_status;
+	}
 
 	int exec_status = regexec(&r, to_match, nmatch, pmatch, 0);
+	regfree(&r);
+
 	if(exec_status != 0) return exec_status;
 
 	*start = pmatch[1].rm_so;
